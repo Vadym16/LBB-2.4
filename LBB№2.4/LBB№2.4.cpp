59d8c354e8bd0de87c56bd6c27d0a38a2f28b025
Vadym16 <117903952+Vadym16@users.noreply.github.com>
2024-11-25 01:32:24 +0100

    Add project files.

---
+++ LBB№2.4/LBB№2.4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void XOR(unsigned char* arr, size_t size);
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+    size_t size;
+    unsigned char arr[] = { 0xff, 0x3D };
+    size = sizeof(arr) / sizeof(arr[0]);
+    cout << "До кодіровки: ";
+    for (size_t i = 0; i < size; i++) {
+        cout << hex << (int)arr[i] << " ";
+    }
+
+    XOR(arr, size);
+
+    cout << "\nПісля: ";
+    for (size_t i = 0; i < size; i++) {
+        cout << hex << (int)arr[i] << " ";
+    }
+
+
+}
+
+void XOR(unsigned char* arr, size_t size) {
+    unsigned char mask = (size % 2 == 0) ? 0x55 : 0xAA;
+
+    for (size_t i = 0; i < size; i++) {
+        arr[i] ^= mask;
+    }
+
+}
